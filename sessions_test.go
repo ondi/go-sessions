305bdfd940753e387c0b19514bf1ec35cf130462
ondi <thy321@gmail.com>
2018-12-26 09:37:15 +0300

    test fix

--- sessions_test.go
+++ sessions_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func ExampleSort1() {
 	var evicted Drop_t
-	cc := NewSessions(1, 15, 10, false, NewDomains(), NoNewData_t{})
+	cc := NewSessions(1, 15, 10, false, NewDomains(), nil)
 	cc.Update(1, 1, 1, 1, &evicted)
 	
 /* Output:
